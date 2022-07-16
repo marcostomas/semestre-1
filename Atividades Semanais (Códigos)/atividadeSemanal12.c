37d8f087b21ccf76db9081ac6861d39706a681ce
marcostomas <mtomasferreira@hotmail.com>
2022-07-16 11:56:24 -0300

    todas atividades programa concluídas

--- Atividades Semanais (Códigos)/atividadeSemanal12.c
+++ Atividades Semanais (Códigos)/atividadeSemanal12.c
@@ -1,154 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int palindromo(char* texto, int n) {
+int palindromo(char *texto, int n)
+{
+    int j = n - 1;
+    int i = 0;
 
-  char* aux = (char*) malloc(sizeof(char)*n);
-
-	for(int i = n;  i < 0; i--)
-	{
-		for(int k = 0; k < n; k++)
-		{
-			aux[i] = texto[k];
-      printf("Valor atribuido a aux[%i] é: %c", i, aux[i]);
-		}
-	}	
-	
-	for(int a = 0; a < n; a++)
-	{
-		if(aux[a] == texto[a])
+    while (i < n)
     {
-      continue;
+        if (texto[i] == texto[j])
+        {
+            i++;
+            j--;
+            continue;
+        }
+        else
+            return 0;
     }
-		else{ 
-      return 0;
-	  }
-  }
-	return 1;
+    return 1;
 }
 
-int main() {
-  char* p1 = (char*) malloc(sizeof(char)*5);
-  p1[0] = 'r';
-  p1[1] = 'a';
-  p1[2] = 'd';
-  p1[3] = 'a';
-  p1[4] = 'r';
+int main()
+{
+    char *p1 = (char *)malloc(sizeof(char) * 5);
+    p1[0] = 'r';
+    p1[1] = 'a';
+    p1[2] = 'd';
+    p1[3] = 'a';
+    p1[4] = 'r';
 
-  if(palindromo(p1,5)){
-    printf("radar eh palindromo!\n");
-  }else{
-    printf("radar nao eh palindromo!\n");
-  }
+    if (palindromo(p1, 5))
+        printf("radar eh palindromo!\n");
+    else
+        printf("radar nao eh palindromo!\n");
 
-  if(palindromo(p1,4)){
-    printf("rada eh palindromo!\n");
-  }else{
-    printf("rada nao eh palindromo!\n");
-  }
+    if (palindromo(p1, 4))
+        printf("rada eh palindromo!\n");
+    else
+        printf("rada nao eh palindromo!\n");
 
-  
-  return 0;
-}#include <stdio.h>
-#include <stdlib.h>
-
-int palindromo(char* texto, int n) {
-
-  char* aux = (char*) malloc(sizeof(char)*n);
-
-	for(int i = n;  i < 0; i--)
-	{
-		for(int k = 0; k < n; k++)
-		{
-			aux[i] = texto[k];
-      printf("Valor atribuido a aux[%i] é: %c", i, aux[i]);
-		}
-	}	
-	
-	for(int a = 0; a < n; a++)
-	{
-		if(aux[a] == texto[a])
-    {
-      continue;
-    }
-		else{ 
-      return 0;
-	  }
-  }
-	return 1;
-}
-
-int main() {
-  char* p1 = (char*) malloc(sizeof(char)*5);
-  p1[0] = 'r';
-  p1[1] = 'a';
-  p1[2] = 'd';
-  p1[3] = 'a';
-  p1[4] = 'r';
-
-  if(palindromo(p1,5)){
-    printf("radar eh palindromo!\n");
-  }else{
-    printf("radar nao eh palindromo!\n");
-  }
-
-  if(palindromo(p1,4)){
-    printf("rada eh palindromo!\n");
-  }else{
-    printf("rada nao eh palindromo!\n");
-  }
-
-  
-  return 0;
-}#include <stdio.h>
-#include <stdlib.h>
-
-int palindromo(char* texto, int n) {
-
-  char* aux = (char*) malloc(sizeof(char)*n);
-
-	for(int i = n;  i < 0; i--)
-	{
-		for(int k = 0; k < n; k++)
-		{
-			aux[i] = texto[k];
-      printf("Valor atribuido a aux[%i] é: %c", i, aux[i]);
-		}
-	}	
-	
-	for(int a = 0; a < n; a++)
-	{
-		if(aux[a] == texto[a])
-    {
-      continue;
-    }
-		else{ 
-      return 0;
-	  }
-  }
-	return 1;
-}
-
-int main() {
-  char* p1 = (char*) malloc(sizeof(char)*5);
-  p1[0] = 'r';
-  p1[1] = 'a';
-  p1[2] = 'd';
-  p1[3] = 'a';
-  p1[4] = 'r';
-
-  if(palindromo(p1,5)){
-    printf("radar eh palindromo!\n");
-  }else{
-    printf("radar nao eh palindromo!\n");
-  }
-
-  if(palindromo(p1,4)){
-    printf("rada eh palindromo!\n");
-  }else{
-    printf("rada nao eh palindromo!\n");
-  }
-
-  
-  return 0;
+    return 0;
 }
---
+++ Atividades Semanais (Códigos)/atividadeSemanal13.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef struct aux
+{
+    float baseMenor;
+    float baseMaior;
+    float altura;
+} TRAPEZIO;
+
+float areaTrapezio1(TRAPEZIO t1)
+{
+
+    return ((t1.baseMaior + t1.baseMenor) * t1.altura / 2);
+}
+
+float areaTrapezio2(TRAPEZIO *r1)
+{
+    return ((r1->baseMaior + r1->baseMenor) * r1->altura / 2);
+}
+
+int main()
+{
+    TRAPEZIO trap;
+    trap.baseMenor = 3.5;
+    trap.baseMaior = 4.5;
+    trap.altura = 2.125;
+    printf("Area do trapezio: %.2f\n", areaTrapezio1(trap));
+
+    printf("Area do trapezio: %.2f\n", areaTrapezio2(&trap));
+
+    return 0;
+}
+
+/* SAIDA
+Area do trapezio: 8.50
+Area do trapezio: 8.50
+*/
